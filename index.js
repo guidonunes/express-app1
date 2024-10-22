6ba7000dc40b7f0450c53d35c072469fbd14c292
Guilherme Nunes <guilherme.augd@gmail.com>
2024-10-22 16:27:15 -0300

    implement routing exercise code

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ var express = require('express');
 app = express();
 
 app.get('/', function(req, res) {
-  res.send('Hello From Express');
+  res.send('Home Page');
+})
+
+app.post('/about', function(req, res) {
+  res.send('About Page');
+})
+
+app.delete('/contact', function(req, res) {
+  res.send('Contact Page');
 })
 
 app.listen(8000, function() {
