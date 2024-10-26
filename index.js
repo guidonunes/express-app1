5afcdd9245679c5d42e2abeb7cce4c7d063336b7
Guilherme Nunes <guilherme.augd@gmail.com>
2024-10-26 15:08:30 -0300

    implement code to get json response

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ app.get('/three', function(req, res) {
   res.status(201).end();
 })
 
+app.get('/four', function(req, res) {
+  let MyJSONArray = [
+    {
+      name: "John",
+      age: 30,
+      city: "New York"
+    },
+    {
+      name: "Smith",
+      age: 50,
+      city: "Chicago"
+    }
+  ];
+
+  res.json(MyJSONArray);
+})
+
 app.post('/two', function(req, res) {
   res.send('About Page');
 })
