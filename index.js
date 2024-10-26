8369985a63791f5c83b50511e0ece2c3b6053a6e
Guilherme Nunes <guilherme.augd@gmail.com>
2024-10-26 15:46:28 -0300

    fix missing variable statement

--- index.js
+++ index.js
@@ -1,34 +1,11 @@
 var express = require('express');
 
-app = express();
+var app = express();
 
-app.get('/three', function(req, res) {
-  res.status(201).end();
-})
 
-app.get('/four', function(req, res) {
-  let MyJSONArray = [
-    {
-      name: "John",
-      age: 30,
-      city: "New York"
-    },
-    {
-      name: "Smith",
-      age: 50,
-      city: "Chicago"
-    }
-  ];
-
-  res.json(MyJSONArray);
-})
-
-app.get('/five', function(req, res) {
-  res.download(201);
-})
 
 app.get('/usa', function(req, res) {
-  res.redirect('https://localhost:8000/india');
+  res.redirect('http://localhost:8000/india');
 })
 
 app.get('/india', function(req, res) {
