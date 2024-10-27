6ceaea2402fe53abba9a97359768f752439bfa3c
Guilherme Nunes <guilherme.augd@gmail.com>
2024-10-27 10:25:04 -0300

    implement code exercise with url query

--- index.js
+++ index.js
@@ -4,44 +4,14 @@ var app = express();
 
 
 
-app.get('/usa', function(req, res) {
-  res.redirect('http://localhost:8000/india');
+app.get('/', function(req, res) {
+  let  firstName = req.query.firstName;
+  let  lastName = req.query.lastName;
+
+  res.end(firstName + ' ' + lastName);
 })
 
-app.get('/india', function(req, res) {
-  res.send('This is India');
-})
 
-app.post('/two', function(req, res) {
-  res.send('About Page');
-})
-
-app.delete('/contact', function(req, res) {
-  res.send('Contact Page');
-})
-
-// Set Status Code
-app.get('/six', function(req, res) {
-  res.append('name', 'Gui N');
-  res.append('city', 'Rio');
-
-  res.status(201).end("Response Header Ended");
-})
-// Set Cookies
-app.get('/seven', function(req, res) {
-  res.cookie('name', 'Gui N');
-  res.cookie('city', 'Rio');
-  res.cookie('age', '33');
-
-  res.end("Cookies Set");
-})
-
-app.get('/eight', function(req, res) {
-  res.clearCookie('name');
-  res.clearCookie('city');
-
-  res.end("Cookies Cleared");
-})
 
 app.listen(8000, function() {
   console.log('Listening on port 8000');
