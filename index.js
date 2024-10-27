f0503f9b33960dda625b36cc8e2685b6e4bf1c4d
Guilherme Nunes <guilherme.augd@gmail.com>
2024-10-27 11:27:14 -0300

    implement code exercise with post request

--- index.js
+++ index.js
@@ -3,12 +3,8 @@ var express = require('express');
 var app = express();
 
 
-
-app.get('/', function(req, res) {
-  let  firstName = req.query.firstName;
-  let  lastName = req.query.lastName;
-
-  res.end(firstName + ' ' + lastName);
+app.post('/', function(req, res) {
+  res.send('Hello World');
 })
 
 
